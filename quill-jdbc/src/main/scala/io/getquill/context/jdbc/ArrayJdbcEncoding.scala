6d866408a7c97b5ee4a3559959f70c5a6267875c
mentegy <mentegy@gmail.com>
2017-04-24 01:42:47 +0300

    todo212123

--- quill-jdbc/src/main/scala/io/getquill/context/jdbc/ArrayJdbcEncoding.scala
+++ quill-jdbc/src/main/scala/io/getquill/context/jdbc/ArrayJdbcEncoding.scala
@@ -1,14 +1,42 @@
 package io.getquill.context.jdbc
 
-import java.sql.{ JDBCType, Types }
-import io.getquill.dsl.TraversableEncoding
+import java.sql.{JDBCType, Types}
+import java.time.LocalDate
+import java.util.{Date, UUID}
+
+import io.getquill.context.sql.dsl.ArrayEncoding
+
 import scala.collection.generic.CanBuildFrom
 
-trait ArrayJdbcEncoding extends TraversableEncoding {
+trait ArrayJdbcEncoding extends ArrayEncoding {
   self: JdbcContext[_, _] =>
 
   implicit def arrayStringEncoder[Col <: Traversable[String]]: Encoder[Col] = rawEncoder[String, Col](Types.VARCHAR)
+  implicit def arrayBigDecimalEncoder[Col <: Traversable[BigDecimal]]: Encoder[Col] = rawEncoder[BigDecimal, Col](Types.NUMERIC)
+  implicit def arrayBooleanEncoder[Col <: Traversable[Boolean]]: Encoder[Col] = rawEncoder[Boolean, Col](Types.BOOLEAN)
+  implicit def arrayByteEncoder[Col <: Traversable[Byte]]: Encoder[Col] = rawEncoder[Byte, Col](Types.TINYINT)
+  implicit def arrayShortEncoder[Col <: Traversable[Short]]: Encoder[Col] = rawEncoder[Short, Col](Types.SMALLINT)
+  implicit def arrayIntEncoder[Col <: Traversable[Int]]: Encoder[Col] = rawEncoder[Int, Col](Types.INTEGER)
+  implicit def arrayLongEncoder[Col <: Traversable[Long]]: Encoder[Col] = rawEncoder[Long, Col](Types.BIGINT)
+  implicit def arrayFloatEncoder[Col <: Traversable[Float]]: Encoder[Col] = rawEncoder[Float, Col](Types.FLOAT)
+  implicit def arrayDoubleEncoder[Col <: Traversable[Double]]: Encoder[Col] = rawEncoder[Double, Col](Types.DOUBLE)
+  implicit def arrayArrayEncoder[Col <: Traversable[Array[Byte]]]: Encoder[Col] = rawEncoder[Array[Byte], Col](Types.VARBINARY)
+  implicit def arrayDateEncoder[Col <: Traversable[Date]]: Encoder[Col] = rawEncoder[Date, Col](Types.TIMESTAMP)
+  implicit def arrayLocalDateEncoder[Col <: Traversable[LocalDate]]: Encoder[Col] = rawEncoder[LocalDate, Col](Types.DATE)
+
   implicit def arrayStringDecoder[Col <: Traversable[String]](implicit bf: CanBuildFrom[Nothing, String, Col]): Decoder[Col] = rawDecoder[String, Col](Types.VARCHAR)
+  implicit def arrayBigDecimalDecoder[Col <: Traversable[BigDecimal]](implicit bf: CBF[BigDecimal, Col]): Decoder[Col] = rawDecoder[BigDecimal, Col](Types.NUMERIC)
+  implicit def arrayBooleanDecoder[Col <: Traversable[Boolean]](implicit bf: CBF[Boolean, Col]): Decoder[Col] = rawDecoder[Boolean, Col](Types.BOOLEAN)
+  implicit def arrayByteDecoder[Col <: Traversable[Byte]](implicit bf: CBF[Byte, Col]): Decoder[Col] = rawDecoder[Byte, Col](Types.TINYINT)
+  implicit def arrayShortDecoder[Col <: Traversable[Short]](implicit bf: CBF[Short, Col]): Decoder[Col] = rawDecoder[Short, Col](Types.SMALLINT)
+  implicit def arrayIntDecoder[Col <: Traversable[Int]](implicit bf: CBF[Int, Col]): Decoder[Col] = rawDecoder[Int, Col](Types.INTEGER)
+  implicit def arrayLongDecoder[Col <: Traversable[Long]](implicit bf: CBF[Long, Col]): Decoder[Col] = rawDecoder[Long, Col](Types.BIGINT)
+  implicit def arrayFloatDecoder[Col <: Traversable[Float]](implicit bf: CBF[Float, Col]): Decoder[Col] = rawDecoder[Float, Col](Types.FLOAT)
+  implicit def arrayDoubleDecoder[Col <: Traversable[Double]](implicit bf: CBF[Double, Col]): Decoder[Col] = rawDecoder[Double, Col](Types.DOUBLE)
+  implicit def arrayArrayDecoder[Col <: Traversable[Array[Byte]]](implicit bf: CBF[Array[Byte], Col]): Decoder[Col] = rawDecoder[Array[Byte], Col](Types.VARBINARY)
+  implicit def arrayDateDecoder[Col <: Traversable[Date]](implicit bf: CBF[Date, Col]): Decoder[Col] = rawDecoder[Date, Col](Types.TIMESTAMP)
+  implicit def arrayLocalDateDecoder[Col <: Traversable[LocalDate]](implicit bf: CBF[LocalDate, Col]): Decoder[Col] = rawDecoder[LocalDate, Col](Types.DATE)
+
 
   private def rawEncoder[T, Col <: Traversable[T]](jdbcType: Int): Encoder[Col] =
     arrayEncoder[T, Col](jdbcType, _.asInstanceOf[AnyRef])
